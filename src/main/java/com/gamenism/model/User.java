1f01caa00dde120b24bddd84b3608a64a58b5d23
Halil Karakose <halilkarakose@gmail.com>
2013-09-28 21:44:30 +0300

    new model classes added

---
+++ src/main/java/com/gamenism/model/User.java
@@ -0,0 +1,47 @@
+package com.gamenism.model;
+
+import javax.persistence.Entity;
+
+/**
+ * User: halil
+ * Date: 9/28/13
+ * Time: 5:39 PM
+ */
+@Entity
+public class User extends BaseEntity {
+    private String email;
+    private String password;
+
+    public User() {
+    }
+
+    public User(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("User{");
+        sb.append("email='").append(email).append('\'');
+        sb.append(", password='").append(password).append('\'');
+        sb.append('}');
+        return sb.toString();
+    }
+}
---
+++ src/test/java/org.bootstrap/EmployeeTest.java
@@ -0,0 +1,46 @@
+package org.bootstrap;
+
+import com.gamenism.dao.EmployeeDao;
+import com.gamenism.model.ActiveRecord;
+import com.gamenism.model.Employee;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import static org.junit.Assert.*;
+
+/**
+ * User: halil
+ * Date: 9/27/13
+ * Time: 11:18 PM
+ */
+public class EmployeeTest extends AbstractTest {
+    @Autowired
+    EmployeeDao employeeDao;
+
+    @Test
+    public void testCreate() {
+        persist();
+    }
+
+    @Test
+    public void testSelect() {
+        Employee persist = persist();
+        Long id = persist.getId();
+        System.out.println("id = " + id);
+        employeeDao.clear();
+
+        Employee employee = employeeDao.find(id);
+        System.out.println("employee = " + employee.getId());
+        assertEquals(id, employee.getId());
+    }
+
+    private Employee persist() {
+        Employee employee = new Employee();
+        employee.setTitle("Mr.");
+        employee.setHiddenValue("Bey");
+        employee.setName("Halil");
+
+        Employee employee1 = employeeDao.create(employee);
+        return employee1;
+    }
+}
