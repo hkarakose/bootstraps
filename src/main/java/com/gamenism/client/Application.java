9f82ceed34d0424c19ff34c261a8bdec1b601d87
Halil Karakose <halilkarakose@gmail.com>
2013-09-28 21:55:58 +0300

    removed rubbish

--- src/main/java/com/gamenism/client/Application.java
+++ src/main/java/com/gamenism/client/Application.java
@@ -1,27 +1,14 @@
 package com.gamenism.client;
 
-import com.gamenism.shared.FieldVerifier;
+import com.gamenism.client.widgets.EmployeeForm;
 import com.google.gwt.core.client.EntryPoint;
-import com.google.gwt.core.client.GWT;
-import com.google.gwt.event.dom.client.ClickEvent;
-import com.google.gwt.event.dom.client.ClickHandler;
-import com.google.gwt.event.dom.client.KeyCodes;
-import com.google.gwt.event.dom.client.KeyUpEvent;
-import com.google.gwt.event.dom.client.KeyUpHandler;
-import com.google.gwt.user.client.rpc.AsyncCallback;
-import com.google.gwt.user.client.ui.Button;
-import com.google.gwt.user.client.ui.DialogBox;
-import com.google.gwt.user.client.ui.HTML;
-import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.RootPanel;
-import com.google.gwt.user.client.ui.TextBox;
-import com.google.gwt.user.client.ui.VerticalPanel;
 
 /**
  * Entry point classes define <code>onModuleLoad()</code>.
  */
 public class Application implements EntryPoint {
     public void onModuleLoad() {
-
+        RootPanel.get().add(LoginView.getInstance());
     }
 }
