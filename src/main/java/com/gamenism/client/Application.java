7f0d47709e3b732d3db2edc41014a43d4722c3c7
Halil Karakose <halilkarakose@gmail.com>
2013-09-30 21:38:24 +0300

    added UncaughtExceptionHandler

--- src/main/java/com/gamenism/client/Application.java
+++ src/main/java/com/gamenism/client/Application.java
@@ -1,7 +1,9 @@
 package com.gamenism.client;
 
-import com.gamenism.client.widgets.EmployeeForm;
+import com.gamenism.client.view.LoginView;
 import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.event.shared.UmbrellaException;
 import com.google.gwt.user.client.ui.RootPanel;
 
 /**
@@ -10,5 +12,22 @@ import com.google.gwt.user.client.ui.RootPanel;
 public class Application implements EntryPoint {
     public void onModuleLoad() {
         RootPanel.get().add(LoginView.getInstance());
+
+        GWT.setUncaughtExceptionHandler(new GWT.UncaughtExceptionHandler() {
+            public void onUncaughtException(Throwable e) {
+                Throwable unwrap = unwrap(e);
+                unwrap.printStackTrace();
+            }
+
+            public Throwable unwrap(Throwable e) {
+                if (e instanceof UmbrellaException) {
+                    UmbrellaException ue = (UmbrellaException) e;
+                    if (ue.getCauses().size() == 1) {
+                        return unwrap(ue.getCauses().iterator().next());
+                    }
+                }
+                return e;
+            }
+        });
     }
 }
