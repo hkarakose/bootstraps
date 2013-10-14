0d17016af74a1c25da7829b091fa72b78da2a320
Halil Karakose <halilkarakose@gmail.com>
2013-10-14 23:27:10 +0300

    organized imports

--- src/main/java/com/gamenism/client/service/UserServiceAsync.java
+++ src/main/java/com/gamenism/client/service/UserServiceAsync.java
@@ -1,11 +1,7 @@
 package com.gamenism.client.service;
 
-import com.gamenism.model.Employee;
 import com.gamenism.model.User;
-import com.google.gwt.core.client.GWT;
 import com.google.gwt.user.client.rpc.AsyncCallback;
-import com.google.gwt.user.client.rpc.RemoteService;
-import com.google.gwt.user.client.rpc.RemoteServiceRelativePath;
 
 public interface UserServiceAsync {
     void create(User user, AsyncCallback<Void> async);
